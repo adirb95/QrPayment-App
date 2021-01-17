d83129992ef33d4024296bd0194603271b48276d
adirb95 <65300958+adirb95@users.noreply.github.com>
2021-01-17 03:49:45 +0200

    new designs and more deleted set content view on MainActivity added time to LoggedInActivity added some changes to MainActivity and MainActivityViewModel

--- app/src/main/java/com/example/qrpayment/MainActivity.java
+++ app/src/main/java/com/example/qrpayment/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
